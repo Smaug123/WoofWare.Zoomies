fd477d906a31d183b2fd90f7d91bed89a835591b
Smaug123 <3138005+Smaug123@users.noreply.github.com>
2025-08-02 20:13:02 +0100

    Strings

--- WoofWare.Zoomies/ConsoleColor.fs
+++ WoofWare.Zoomies/ConsoleColor.fs
@@ -2,7 +2,7 @@ namespace WoofWare.Zoomies
 
 open System
 
-// Microsoft, probably: "Don't worry your pretty little head about this, Microsoft's gonna take care of you"
+// Microsoft, probably: @"Don't worry your pretty little head about this, Microsoft's gonna take care of you"
 
 [<RequireQualifiedAccess>]
 module ConsoleColor =
@@ -11,31 +11,31 @@ module ConsoleColor =
     // if only they saw fit to make this public
     let toForegroundEscapeCode (c : ConsoleColor) : string =
         match c with
-        | ConsoleColor.Black -> "\e[30m"
-        | ConsoleColor.DarkRed -> "\e[31m"
-        | ConsoleColor.DarkGreen -> "\e[32m"
-        | ConsoleColor.DarkYellow -> "\e[33m"
-        | ConsoleColor.DarkBlue -> "\e[34m"
-        | ConsoleColor.DarkMagenta -> "\e[35m"
-        | ConsoleColor.DarkCyan -> "\e[36m"
-        | ConsoleColor.Gray -> "\e[37m"
-        | ConsoleColor.Red -> "\e[1m\e[31m"
-        | ConsoleColor.Green -> "\e[1m\e[32m"
-        | ConsoleColor.Yellow -> "\e[1m\e[33m"
-        | ConsoleColor.Blue -> "\e[1m\e[34m"
-        | ConsoleColor.Magenta -> "\e[1m\e[35m"
-        | ConsoleColor.Cyan -> "\e[1m\e[36m"
-        | ConsoleColor.White -> "\e[1m\e[37m"
-        | _ -> "\e[39m\e[22m" // default foreground
+        | ConsoleColor.Black -> @"\e[30m"
+        | ConsoleColor.DarkRed -> @"\e[31m"
+        | ConsoleColor.DarkGreen -> @"\e[32m"
+        | ConsoleColor.DarkYellow -> @"\e[33m"
+        | ConsoleColor.DarkBlue -> @"\e[34m"
+        | ConsoleColor.DarkMagenta -> @"\e[35m"
+        | ConsoleColor.DarkCyan -> @"\e[36m"
+        | ConsoleColor.Gray -> @"\e[37m"
+        | ConsoleColor.Red -> @"\e[1m\e[31m"
+        | ConsoleColor.Green -> @"\e[1m\e[32m"
+        | ConsoleColor.Yellow -> @"\e[1m\e[33m"
+        | ConsoleColor.Blue -> @"\e[1m\e[34m"
+        | ConsoleColor.Magenta -> @"\e[1m\e[35m"
+        | ConsoleColor.Cyan -> @"\e[1m\e[36m"
+        | ConsoleColor.White -> @"\e[1m\e[37m"
+        | _ -> @"\e[39m\e[22m" // default foreground
 
     let toBackgroundEscapeCode (c : ConsoleColor) : string =
         match c with
-        | ConsoleColor.Black -> "\e[40m"
-        | ConsoleColor.DarkRed -> "\e[41m"
-        | ConsoleColor.DarkGreen -> "\e[42m"
-        | ConsoleColor.DarkYellow -> "\e[43m"
-        | ConsoleColor.DarkBlue -> "\e[44m"
-        | ConsoleColor.DarkMagenta -> "\e[45m"
-        | ConsoleColor.DarkCyan -> "\e[46m"
-        | ConsoleColor.Gray -> "\e[47m"
-        | _ -> "\e[49m" // default background
+        | ConsoleColor.Black -> @"\e[40m"
+        | ConsoleColor.DarkRed -> @"\e[41m"
+        | ConsoleColor.DarkGreen -> @"\e[42m"
+        | ConsoleColor.DarkYellow -> @"\e[43m"
+        | ConsoleColor.DarkBlue -> @"\e[44m"
+        | ConsoleColor.DarkMagenta -> @"\e[45m"
+        | ConsoleColor.DarkCyan -> @"\e[46m"
+        | ConsoleColor.Gray -> @"\e[47m"
+        | _ -> @"\e[49m" // default background
