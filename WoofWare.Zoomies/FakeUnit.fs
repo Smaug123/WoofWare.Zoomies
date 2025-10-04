be251c479f195934c15dbf6b90a6a36fa3aef689
Patrick Stevens <3138005+Smaug123@users.noreply.github.com>
2025-10-04 11:15:41 +0100

    Delete WoofWare.Zoomies/FakeUnit.fs

--- WoofWare.Zoomies/FakeUnit.fs
+++
@@ -1,9 +0,0 @@
-namespace WoofWare.Zoomies
-
-type internal FakeUnit = private | FakeUnit
-
-[<RequireQualifiedAccess>]
-module internal FakeUnit =
-
-    let fake () = FakeUnit
-    let unfake (f : FakeUnit) = ()
